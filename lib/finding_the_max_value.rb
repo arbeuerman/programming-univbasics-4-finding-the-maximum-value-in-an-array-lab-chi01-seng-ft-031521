059d644c0319c3914481d954b3bcd0fb880fa54b
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-22 18:03:24 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,17 +1,26 @@
+require 'pry'
+
 def find_max_value(array)
   max_value = nil 
   length = array.length 
   length.times do |entry|
     #get each value 
     current_value = array[entry]
+    binding.pry 
     #check current value against every other value
     length.times do |next_value|
       if current_value < array[next_value]
+        binding.pry
         break
       else 
         max_value = current_value
+        binding.pry
       end 
     end 
   end
+  binding.pry
   max_value
 end
+
+
+puts find_max_value([1,2,5,3,3,8,2,1])
